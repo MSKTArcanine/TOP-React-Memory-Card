e4b31aeac5dd916571252982dcfb0a9007debf48
MSKTArcanine <marcantoine1999@live.fr>
2025-05-20 13:24:28 +0200

    useEffect fetch to API done. TODO : implementing the game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
-function App() {
+export default function App() {
+  const [pokemonList, setPokemonList] = useState(['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise'])
+  const didFetch = useRef(false);
+  // MARCHE MAIS STOP SPAM LES APIs.
+  // useEffect(() => {
+  //   if(!didFetch.current){
+  //    const promises = [];
+  //    for(let i = 1; i <= 9; i++){
+  //     promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`))
+  //    }
+  //    Promise.all(promises)
+  //    .then(results => Promise.all(results.map(r => r.json())))
+  //    .then(dataArray => {
+  //     const names = dataArray.map(data => data.name);
+  //     setPokemonList(names);
+  //    })
+     
+  //     didFetch.current = true;
+  //   }
+  // }, [])
 
+  console.log(pokemonList)
   return (
     <>
     </>
   )
 }
-
-export default App
